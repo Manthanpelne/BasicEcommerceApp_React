d5eba44bc284147ba49af6cc077a7c535ac22041
Manthanpelne <111408525+Manthanpelne@users.noreply.github.com>
2024-02-18 10:56:17 +0530

    basic react E-commerce App

---
+++ ecommerce-app/src/products.js
@@ -0,0 +1,47 @@
+
+import product4 from "./assets/4.png";
+import product5 from "./assets/5.png";
+import product6 from "./assets/6.webp";
+import product1 from "./assets/1.png";
+import product2 from "./assets/2.png";
+import product3 from "./assets/3.png";
+
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Denim Jacket",
+    price: 250,
+    productImage: product4,
+  },
+  {
+    id: 2,
+    productName: "Apple Watch",
+    price: 10000,
+    productImage: product5,
+  },
+  {
+    id: 3,
+    productName: "H&M Hoodie",
+    price: 450,
+    productImage: product6,
+  },
+    {
+      id: 4,
+      productName: "IPhone",
+      price: 40000,
+      productImage: product1,
+    },
+    {
+      id: 5,
+      productName: "Macbook Pro",
+      price: 25000,
+      productImage: product2,
+    },
+    {
+      id: 6,
+      productName: "Cannon Camera",
+      price: 5000,
+      productImage: product3,
+    }
+  ];
